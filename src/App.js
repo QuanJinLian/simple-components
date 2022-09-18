a2cd2293ac5248f0db2f2e05a51a2edfdfcddd45
gwongeumlyeon <jinlian.quan@atto-research.com>
2022-09-18 15:22:29 +0900

    create project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  return <div className="container">Hello, This is smiple components</div>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'main.js',
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(jpeg|jpg|png| svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: 'src/asset', to: 'asset' }],
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    open: true,
+    port: 3005,
+    overlay: true,
+    hot: true,
+  },
+};
