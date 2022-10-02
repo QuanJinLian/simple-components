2f3ad6552956b34b7753362ac79ab802a240ec7e
gwongeumlyeon <jinlian.quan@atto-research.com>
2022-10-02 15:42:32 +0900

    children 이 span으로 잘못 감싸지는 버그 수정

--- src/pages/tooltip/TooltipPage.js
+++ src/pages/tooltip/TooltipPage.js
@@ -22,77 +22,98 @@ const text = (
 const buttonWidth = 70;
 export function TooltipPage() {
   return (
-    <div className="demo" style={{ marginTop: '100px', marginLeft: '100px' }}>
-      <div
-        style={{
-          marginLeft: buttonWidth,
-          whiteSpace: 'nowrap',
-        }}
-      >
-        <Tooltip placement="top-left" title={text}>
-          {/*<button>TL</button>*/}
-          <TextTest />
-        </Tooltip>
-        <Tooltip placement="top" title={text}>
-          <button>Top</button>
-        </Tooltip>
-        <Tooltip placement="top-right" title={text}>
-          <button>TR</button>
-        </Tooltip>
+    <>
+      <div className="demo" style={{ marginTop: '100px', marginLeft: '100px' }}>
+        <div
+          className="top"
+          style={{
+            marginLeft: buttonWidth,
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <Tooltip placement="top-left" title={text}>
+            {/*<button>TL</button>*/}
+            <TextTest />
+          </Tooltip>
+          <Tooltip placement="top" title={text}>
+            <button>Top</button>
+          </Tooltip>
+          <Tooltip placement="top-right" title={text}>
+            <button>TR</button>
+          </Tooltip>
+        </div>
+        <div
+          style={{
+            width: buttonWidth,
+            float: 'left',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Tooltip placement="left-top" title={text}>
+            <button>LT</button>
+          </Tooltip>
+          <Tooltip placement="left" title={text}>
+            <button>Left</button>
+          </Tooltip>
+          <Tooltip placement="left-bottom" title={text}>
+            <button>LB</button>
+          </Tooltip>
+        </div>
+        <div
+          style={{
+            width: buttonWidth,
+            marginLeft: buttonWidth * 4,
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <Tooltip placement="right-top" title={text}>
+            <button>RT</button>
+          </Tooltip>
+          <Tooltip placement="right" title={text}>
+            <button>Right</button>
+          </Tooltip>
+          <Tooltip placement="right-bottom" title={text}>
+            <button>RB</button>
+          </Tooltip>
+        </div>
+        <div
+          style={{
+            marginLeft: buttonWidth,
+            clear: 'both',
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <Tooltip placement="bottom-left" title={text}>
+            <button>BL</button>
+          </Tooltip>
+          <Tooltip placement="bottom" title={text}>
+            <button>Bottom</button>
+          </Tooltip>
+          <Tooltip placement="bottom-right" title={text}>
+            <button>BR</button>
+          </Tooltip>
+        </div>
       </div>
-      <div
-        style={{
-          width: buttonWidth,
-          float: 'left',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <Tooltip placement="left-top" title={text}>
-          <button>LT</button>
-        </Tooltip>
-        <Tooltip placement="left" title={text}>
-          <button>Left</button>
-        </Tooltip>
-        <Tooltip placement="left-bottom" title={text}>
-          <button>LB</button>
-        </Tooltip>
+      <div className="demo2" style={{ marginTop: '50px', marginLeft: '100px' }}>
+        <div
+          style={{
+            marginLeft: buttonWidth,
+            whiteSpace: 'nowrap',
+          }}
+        >
+          <Tooltip placement="top-left" title={text} overlayClassName="enter-delay">
+            <button>enter-delay 1s</button>
+          </Tooltip>
+          <Tooltip placement="top" title={text} overlayClassName="leave-delay">
+            <button>leave-delay 1s</button>
+          </Tooltip>
+          <Tooltip placement="top-right" title={text} overlayClassName="hover-view">
+            <button>hover not hidden</button>
+          </Tooltip>
+        </div>
       </div>
-      <div
-        style={{
-          width: buttonWidth,
-          marginLeft: buttonWidth * 4,
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <Tooltip placement="right-top" title={text}>
-          <button>RT</button>
-        </Tooltip>
-        <Tooltip placement="right" title={text}>
-          <button>Right</button>
-        </Tooltip>
-        <Tooltip placement="right-bottom" title={text}>
-          <button>RB</button>
-        </Tooltip>
-      </div>
-      <div
-        style={{
-          marginLeft: buttonWidth,
-          clear: 'both',
-          whiteSpace: 'nowrap',
-        }}
-      >
-        <Tooltip placement="bottom-left" title={text}>
-          <button>BL</button>
-        </Tooltip>
-        <Tooltip placement="bottom" title={text}>
-          <button>Bottom</button>
-        </Tooltip>
-        <Tooltip placement="bottom-right" title={text}>
-          <button>BR</button>
-        </Tooltip>
-      </div>
-    </div>
+    </>
   );
 }
