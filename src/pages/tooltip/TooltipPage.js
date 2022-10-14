f35eb1191bb79377bfb22e8c26ea0cfa5e04ce38
gwongeumlyeon <jinlian.quan@atto-research.com>
2022-10-14 19:36:10 +0900

    tooltip button 스타일 수정

--- src/pages/tooltip/TooltipPage.js
+++ src/pages/tooltip/TooltipPage.js
@@ -4,7 +4,7 @@ import { Tooltip } from '../../components';
 
 const InnerComponent = () => {
   return (
-    <div className="inner">
+    <div className="test-button">
       <div>TL</div>
       <div>TL</div>
       <div>TL</div>
@@ -18,103 +18,72 @@ const TextTest = () => {
 
 const text = (
   <div>
-    <span style={{ display: 'flex' }}>prompt text </span>
-    <span style={{ display: 'flex' }}>prompt text </span>
-    <span style={{ display: 'flex' }}>prompt text </span>
+    <span className="prompt-text__span">prompt text </span>
+    <span className="prompt-text__span">prompt text </span>
+    <span className="prompt-text__span">prompt text </span>
   </div>
 );
-const buttonWidth = 70;
+
 export function TooltipPage() {
   return (
     <>
-      <div className="demo" style={{ marginTop: '100px', marginLeft: '100px' }}>
-        <div
-          className="top"
-          style={{
-            marginLeft: buttonWidth,
-            whiteSpace: 'nowrap',
-          }}
-        >
+      <div className="demo">
+        <div className="top-wrapper">
           <Tooltip placement="top-left" title={text}>
             {/*<button>TL</button>*/}
             <TextTest />
           </Tooltip>
           <Tooltip placement="top" title={text}>
-            <button>Top</button>
+            <button className="test-button">Top</button>
           </Tooltip>
           <Tooltip placement="top-right" title={text}>
-            <button>TR</button>
+            <button className="test-button">TR</button>
           </Tooltip>
         </div>
-        <div
-          style={{
-            width: buttonWidth,
-            float: 'left',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
+        <div className="left-wrapper">
           <Tooltip placement="left-top" title={text}>
-            <button>LT</button>
+            <button className="test-button">LT</button>
           </Tooltip>
           <Tooltip placement="left" title={text}>
-            <button>Left</button>
+            <button className="test-button">Left</button>
           </Tooltip>
           <Tooltip placement="left-bottom" title={text}>
-            <button>LB</button>
+            <button className="test-button">LB</button>
           </Tooltip>
         </div>
-        <div
-          style={{
-            width: buttonWidth,
-            marginLeft: buttonWidth * 4,
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
+        <div className="right-wrapper">
           <Tooltip placement="right-top" title={text}>
-            <button>RT</button>
+            <button className="test-button">RT</button>
           </Tooltip>
           <Tooltip placement="right" title={text}>
-            <button>Right</button>
+            <button className="test-button">Right</button>
           </Tooltip>
           <Tooltip placement="right-bottom" title={text}>
-            <button>RB</button>
+            <button className="test-button">RB</button>
           </Tooltip>
         </div>
-        <div
-          style={{
-            marginLeft: buttonWidth,
-            clear: 'both',
-            whiteSpace: 'nowrap',
-          }}
-        >
+        <div className="bottom-wrapper">
           <Tooltip placement="bottom-left" title={text}>
-            <button>BL</button>
+            <button className="test-button">BL</button>
           </Tooltip>
           <Tooltip placement="bottom" title={text}>
-            <button>Bottom</button>
+            <button className="test-button">Bottom</button>
           </Tooltip>
           <Tooltip placement="bottom-right" title={text}>
-            <button>BR</button>
+            <button className="test-button">BR</button>
           </Tooltip>
         </div>
       </div>
-      <div className="demo2" style={{ marginTop: '50px', marginLeft: '100px' }}>
-        <div
-          style={{
-            marginLeft: buttonWidth,
-            whiteSpace: 'nowrap',
-          }}
-        >
+      <div className="demo2">
+        <div>
           <Tooltip placement="top-left" title={text} overlayClassName="enter-delay">
-            <button>enter-delay 1s</button>
+            <button className="test-button">enter-delay 1s</button>
           </Tooltip>
           <Tooltip placement="top" title={text} overlayClassName="leave-delay">
-            <button>leave-delay 1s</button>
+            <button className="test-button">leave-delay 1s</button>
           </Tooltip>
           <Tooltip placement="top-right" title={text} overlayClassName="hover-view">
-            <button>hover not hidden</button>
+            <button className="test-button">hover not hidden</button>
           </Tooltip>
         </div>
       </div>
