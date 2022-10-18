a04b62c8cf45dfd4d2a3146f528e285a34b37cba
QuanJinLian <micka772@naver.com>
2022-10-18 23:07:56 +0900

    토스트 예제 페이지 개선

--- src/pages/toast/ToastPage.js
+++ src/pages/toast/ToastPage.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Toast, toastService } from '../../components';
+import { Toast, toastService, Tooltip } from '../../components';
+
+const TextTooltip = () => {
+  return (
+    <>
+      <div>20초짜리 Error 토스트</div>
+      <div>close 시 5초짜리 Success 토스트 뜸 </div>
+    </>
+  );
+};
 
 let num = 0;
 export function ToastPage() {
@@ -17,22 +26,30 @@ export function ToastPage() {
     <>
       <Toast />
       <div className="show-toast-button-wrapper">
-        <button className="show-toast-button normal" style={{ width: '100px' }} onClick={() => showToast(1)}>
-          normal toast
-        </button>
-        <button className="show-toast-button warning" style={{ width: '100px' }} onClick={() => showToast(2)}>
-          waring toast
-        </button>
-        <button className="show-toast-button error" style={{ width: '100px' }} onClick={() => showToast(3)}>
-          error toast
-        </button>
-        <button
-          className="show-toast-button error"
-          style={{ width: '100px' }}
-          onClick={() => showToast(3, 20, onClose)}
-        >
-          error toast 20s
-        </button>
+        <Tooltip title="5초짜리 Success 토스트">
+          <button className="show-toast-button normal" style={{ width: '100px' }} onClick={() => showToast(1)}>
+            normal toast
+          </button>
+        </Tooltip>
+        <Tooltip title="5초짜리 Warning 토스트">
+          <button className="show-toast-button warning" style={{ width: '100px' }} onClick={() => showToast(2)}>
+            waring toast
+          </button>
+        </Tooltip>
+        <Tooltip title="5초짜리 Error 토스트">
+          <button className="show-toast-button error" style={{ width: '100px' }} onClick={() => showToast(3)}>
+            error toast
+          </button>
+        </Tooltip>
+        <Tooltip title={TextTooltip()} placement="bottom-right">
+          <button
+            className="show-toast-button error"
+            style={{ width: '100px' }}
+            onClick={() => showToast(3, 20, onClose)}
+          >
+            error toast 20s
+          </button>
+        </Tooltip>
       </div>
     </>
   );
